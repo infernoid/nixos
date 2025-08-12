469938ff9bd82e6e1cc703b2c6ef2977a6b89a38
infernoid <infernoid@gmail.com>
2025-08-12 21:52:02 +0200

    update

--- home/alacritty.nix
+++ home/alacritty.nix
@@ -6,34 +6,50 @@
 
         enable = true;
 
-	settings = {
-
-            general = {
-	        import = [
-	    	    "alacritty-dracula-theme.yml"
-                ];
-	    };
+	setting = {
 
             font = {
-                size = 14;
+                size = 12.0;
                 normal = {
                     family = "JetBrainsMono Nerd Font";
-                    style = "Regular";
                 };
-                bold = {
-                    family = "JetBrainsMono Nerd Font";
-                    style = "Bold";
-                };
-                italic = {
-                    family = "JetBrainsMono Nerd Font";
-                    style = "Italic";
-                };
-                bold_italic = {
-                    family = "JetBrainsMono Nerd Font";
-                    style = "Bold Italic";
-                };
+            };
 
-            }; # font
+            # The Dracula theme colors
+            colors = {
+                # Default colors
+                primary = {
+                    background = "#282a36";
+                    foreground = "#f8f8f2";
+                };
+                # Cursor colors
+                cursor = {
+                    text = "#282a36";
+                    cursor = "#f8f8f2";
+                };
+                # Normal colors
+                normal = {
+                    black   = "#21222c";
+                    red     = "#ff5555";
+                    green   = "#50fa7b";
+                    yellow  = "#f1fa8c";
+                    blue    = "#bd93f9";
+                    magenta = "#ff79c6";
+                    cyan    = "#8be9fd";
+                    white   = "#f8f8f2";
+                };
+                # Bright colors
+                bright = {
+                    black   = "#6272a4";
+                    red     = "#ff6e6e";
+                    green   = "#69ff94";
+                    yellow  = "#ffffa5";
+                    blue    = "#d6acff";
+                    magenta = "#ff92df";
+                    cyan    = "#a4ffff";
+                    white   = "#ffffff";
+                };
+            };
 
         };
     };
