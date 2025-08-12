880ab9a8587922f05005ecfa29f07b4e534075b3
infernoid <infernoid@gmail.com>
2025-08-12 19:39:15 +0200

    update

--- flake.nix
+++ flake.nix
@@ -19,7 +19,7 @@
         
     };
 
-    outputs = { self, nixpkgs, home-manager, stylix, ... }@inputs: let
+    outputs = { self, nixpkgs, home-manager, ... }@inputs: let
     
         system = "x86_64-linux";
         pkgs = import nixpkgs { inherit system; };   
