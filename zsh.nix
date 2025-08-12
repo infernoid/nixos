4306f7e8e435feb9369330457ca360354411273a
infernoid <infernoid@gmail.com>
2025-08-12 23:24:40 +0200

    oh my zsh

--- zsh.nix
+++ zsh.nix
@@ -5,9 +5,19 @@
     # enable ZSH
     programs.zsh = {
         enable = true;
+
         enableCompletion = true;
+        autosuggestions.enable = true;
+        syntaxHighlighting.enable = true;
+
+        oh-my-zsh = {
+           enable = true;
+           plugins = [ "git" ];
+        };
+
     };
 
     environment.shells = with pkgs; [ zsh ];
+    # users.defaultUserShell = pkgs.zsh;
 
 }
